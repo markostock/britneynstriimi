86de17c9245ce80971479485b285e3b038324e59
markostock <165729199+markostock@users.noreply.github.com>
2024-05-12 20:02:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 
 //Kolikot
     "🪙","🪙","🪙","🪙","🪙","🪙","🪙","🪙","🪙",
-//Voice changer
-    "🎙️","🎙️","🎙️","🎙️","🎙️","🎙️","🎙️","🎙️","🎙️",
+//Voice changer ja pyöräytys
+    "🧁","🧁","🧁","🧁","🧁","🧁","🧁","🧁","🧁",
 //Poison
     "🍄","🍄","🍄","🍄","🍄","🍄","🍄","🍄","🍄",
 //peruukki pois 30min+1h
@@ -107,3 +107,63 @@
 
   init();
 })();
+
+// Dice
+
+const dice = document.querySelector('.dice');
+const rollBtn = document.querySelector('.roll');
+
+const randomDice = () => {
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random >= 1 && random <= 6) {
+        rollDice(random);
+    }
+    else {
+        randomDice();
+    }
+}
+
+const rollDice = random => {
+
+    dice.style.animation = 'rolling 4s';
+
+    setTimeout(() => {
+
+        switch (random) {
+            case 1:
+                dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+
+            case 6:
+                dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+
+            case 2:
+                dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                break;
+
+            case 5:
+                dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+
+            case 3:
+                dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                break;
+
+            case 4:
+                dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                break;
+
+            default:
+                break;
+        }
+
+        dice.style.animation = 'none';
+
+    }, 4050);
+
+}
+
+rollBtn.addEventListener('click', randomDice);
